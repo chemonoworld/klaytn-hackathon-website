3fd12a7f5acfab1e181b03a6d0f05b45f1ac34ef
LAPTOP-7G174PE1\이진우 <jinwoo0451@naver.com>
2022-09-22 02:42:58 +0900

    refactor main page and soul page

--- src/pages/SoulPage/index.tsx
+++ src/pages/SoulPage/index.tsx
@@ -3,6 +3,7 @@ import Button from '../../components/Button';
 import SBT from '../../assets/images/sbt-256.png';
 import SOUL from '../../assets/images/soul-256.png';
 import '../MainPage/mainpage.scss';
+import { Link } from 'react-router-dom';
 
 type mainContentProps = {
   // true : SBT false: SOUL
@@ -12,55 +13,38 @@ type mainContentProps = {
 const MainPage = ({ mainContent }: mainContentProps) => {
   const [SBTorSOUL, setSBTorSOUL] = useState(mainContent);
   const [isLogoTranslate, setIsLogoTranslate] = useState(false);
+  const [isJoystickTrans, setIsJoystickTrans] = useState(false);
   useEffect(() => {
+    if (!isJoystickTrans) {
+      const joystick = document.querySelector('.btn-joystick');
+      joystick?.setAttribute('class', 'btn-joystick btn-joystick-clicked');
+    }
     if (!isLogoTranslate) {
       const mainLogo = document.querySelector('.main-logo');
       mainLogo?.setAttribute('class', 'main-logo main-logo-appear');
     }
-  }, [isLogoTranslate]);
+  }, [isJoystickTrans, isLogoTranslate]);
+  const startJoystickAnim = () => {
+    const joystick = document.querySelector('.btn-joystick');
+    joystick?.setAttribute('class', 'btn-joystick btn-joystick-clicked');
+    setIsJoystickTrans(true);
+    const joystickTimer = setTimeout(() => {
+      joystick?.setAttribute('class', 'btn-joystick');
+      setIsJoystickTrans(false);
+      clearTimeout(joystickTimer);
+    }, 500);
+  };
   const handleSlide = () => {
     const mainLogo = document.querySelector('.main-logo');
     mainLogo?.setAttribute('class', 'main-logo main-logo-init');
     setIsLogoTranslate(true);
-    const joystick = document.querySelector('.btn-joystick');
-    joystick?.setAttribute('class', 'btn-joystick btn-joystick-clicked');
     const logoTimer = setTimeout(() => {
       const mainLogo = document.querySelector('.main-logo');
       mainLogo?.setAttribute('class', 'main-logo');
       setIsLogoTranslate(false);
       clearTimeout(logoTimer);
-    }, 200);
-    const joystickTimer = setTimeout(() => {
-      joystick?.setAttribute('class', 'btn-joystick');
-      clearTimeout(joystickTimer);
     }, 500);
-    if (SBTorSOUL) {
-      setSBTorSOUL(false);
-      const btn = document.querySelectorAll('.btn-main-section')[0];
-      const whiteBtn = document.querySelectorAll('.btn-main-section')[1];
-      const textInPinkBtn = btn.querySelector('span');
-      const textInWhiteBtn = whiteBtn.querySelector('span');
-      btn?.setAttribute('class', 'button btn-main-section btn-red');
-      textInPinkBtn?.innerText
-        ? (textInPinkBtn.innerText = 'Klaim your soul')
-        : alert('button color shift error');
-      textInWhiteBtn?.innerText
-        ? (textInWhiteBtn.innerText = 'Download Extension')
-        : alert('button color shift error');
-    } else {
-      setSBTorSOUL(true);
-      const btn = document.querySelectorAll('.btn-main-section')[0];
-      const whiteBtn = document.querySelectorAll('.btn-main-section')[1];
-      const textInRedBtn = btn.querySelector('span');
-      const textInWhiteBtn = whiteBtn.querySelector('span');
-      btn?.setAttribute('class', 'button btn-main-section');
-      textInRedBtn?.innerText
-        ? (textInRedBtn.innerText = 'Learn more')
-        : alert('button color shift error');
-      textInWhiteBtn?.innerText
-        ? (textInWhiteBtn.innerText = 'Launch app')
-        : alert('button color shift error');
-    }
+    startJoystickAnim();
   };
   const handleClickMainBtns = () => {
     alert('click button');
@@ -75,85 +59,50 @@ const MainPage = ({ mainContent }: mainContentProps) => {
             ) : (
               <img src={SOUL} className="main-logo logo-sbt" alt="logo" />
             )}
-            <div onClick={handleSlide} className="btn-joystick"></div>
+            <Link to="/">
+              <div onClick={handleSlide} className="btn-joystick"></div>
+            </Link>
           </div>
           <div className="main-section-content explanation-box">
             <span className="explanation-title">FEATURE</span>
-            {SBTorSOUL === true ? (
-              <ol>
-                <li>
-                  <span>
-                    <span className="txt-pink">
-                      0x
-                      <span className="txt-hover-cyan">SBT</span>
-                    </span>{' '}
-                    is a DAO which Evaluates other DAOs
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    All DAOs can be evaluated from other DAOs in
-                    <span> </span>
-                    <span className="txt-pink">
-                      0x
-                      <span className="txt-hover-cyan">SBT</span>
-                    </span>
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    The{' '}
-                    <span className="txt-pink txt-hover-cyan">Governance</span>{' '}
-                    is based on Soul-Bound Tokens
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    The <span className="txt-pink txt-hover-cyan">Rating</span>{' '}
-                    is stored on Klaytn Blockchain
-                  </span>
-                </li>
-              </ol>
-            ) : (
-              <ol>
-                <li>
-                  <span>
-                    <span className="txt-red">
-                      0x
-                      <span className="txt-hover-cyan">SOUL</span>
-                    </span>{' '}
-                    is a SBT to move your soul to Twitter
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    <span className="txt-red txt-hover-cyan">SOUL</span> is
-                    connected to your address and Twitter ID
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Extension shows your{' '}
-                    <span className="txt-red txt-hover-cyan">SOUL</span> on
-                    Twitter
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Extension shows the list of{' '}
-                    <span className="txt-red txt-hover-cyan">DAOs</span> you
-                    belong to
-                  </span>
-                </li>
-                <li>
-                  <span className="new-line-float-left">
-                    Extension show the{' '}
-                    <span className="txt-red txt-hover-cyan">rating</span> of
-                    the DAOs you belong to
-                  </span>
-                </li>
-              </ol>
-            )}
+            <ol>
+              <li>
+                <span>
+                  <span className="txt-red">
+                    0x
+                    <span className="txt-hover-cyan">SOUL</span>
+                  </span>{' '}
+                  is a SBT to move your soul to Twitter
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="txt-red txt-hover-cyan">SOUL</span> is
+                  connected to your address and Twitter ID
+                </span>
+              </li>
+              <li>
+                <span>
+                  Extension shows your{' '}
+                  <span className="txt-red txt-hover-cyan">SOUL</span> on
+                  Twitter
+                </span>
+              </li>
+              <li>
+                <span>
+                  Extension shows the list of{' '}
+                  <span className="txt-red txt-hover-cyan">DAOs</span> you
+                  belong to
+                </span>
+              </li>
+              <li>
+                <span className="new-line-float-left">
+                  Extension show the{' '}
+                  <span className="txt-red txt-hover-cyan">rating</span> of the
+                  DAOs you belong to
+                </span>
+              </li>
+            </ol>
           </div>
         </div>
       </section>
