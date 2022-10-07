eb2e21e48b533fd6fd1a62052fcb907d72c941d1
LAPTOP-7G174PE1\이진우 <jinwoo0451@naver.com>
2022-10-07 23:09:33 +0900

    add react-modal pkg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+
 import './App.scss';
 import Layout from './components/Layout';
 import MainPage from './pages/MainPage';
